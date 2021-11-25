b31c2de80cac51c87f72771f1718133adfcf1a6a
a.skrichevskij <skrtoha@mebot.ru>
2021-11-25 23:08:40 +0200

    Импорт

--- local/modules/dev.import/lib/import.php
+++ local/modules/dev.import/lib/import.php
@@ -4,6 +4,7 @@ namespace  Dev\Import;
 use Bitrix\Catalog\Model\Price;
 use Bitrix\Catalog\Model\Product;
 use Bitrix\Main\Loader;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/vendor/autoload.php");
 Loader::includeModule('iblock');
@@ -12,13 +13,14 @@ class Import{
     private $iterator;
     
     public function __construct($filePath){
-        $xls = \PhpOffice\PhpSpreadsheet\IOFactory::load($filePath);
+        $xls = IOFactory::load($filePath);
         $xls->setActiveSheetIndex(0);
         $sheet = $xls->getActiveSheet();
         $this->iterator = $sheet->getRowIterator();
     }
     
     public function execute(){
+        $data = [];
         foreach ($this->iterator as $iterator) {
             $row = array();
             $cellIterator = $iterator->getCellIterator();
@@ -26,25 +28,44 @@ class Import{
                 $row[] = $cell->getCalculatedValue();
             }
             if ($row[4] == 'Номенклатура' || !$row[4]) continue;
+            $data[] = $row;
+        }
+        
+        $countData = count($data);
+        for($i = 0; $i < $countData; $i++) {
+            $row = $data[$i];
+            $nextRow = $data[$i + 1];
             if (self::isSection($row)){
                 $section = \CIBlockSection::GetList([], [
                     'NAME' => $row[4]
                 ])->Fetch();
-                if (empty($section)){
-                    $sectionObject = new \CIBlockSection();
-                    $section_id = $sectionObject->Add([
-                        'NAME' => $row[4],
-                        'IBLOCK_ID' => 2,
-                        'CODE' => \CUtil::translit($row[4], 'ru')
-                    ]);
-                } else{
+                if(empty($section)){
+                    if (self::isSection($row) && self::isSection($nextRow)){
+                        $mainSectionObject = new \CIBlockSection();
+                        $main_section_id = $mainSectionObject->Add([
+                            'NAME' => $row[4],
+                            'IBLOCK_ID' => 2,
+                            'CODE' => \CUtil::translit($row[4], 'ru'),
+                        ]);
+                        continue;
+                    }
+                    if (self::isSection($row)){
+                        $sectionObject = new \CIBlockSection();
+                        $section_id = $sectionObject->Add([
+                            'NAME' => $row[4],
+                            'IBLOCK_ID' => 2,
+                            'CODE' => \CUtil::translit($row[4], 'ru'),
+                            'IBLOCK_SECTION_ID' => $main_section_id
+                        ]);
+                        continue;
+                    }
+                }
+                else{
+                    if (!$section['IBLOCK_SECTION_ID']) continue;
                     $section_id = $section['ID'];
                 }
-                continue;
             }
-            
-            if (!$section_id) continue;
-            
+           
             $iblock = new \CIBlockElement();
             $product_id = $iblock->Add([
                 'IBLOCK_ID' => 2,
@@ -53,14 +74,14 @@ class Import{
                 'IBLOCK_SECTION_ID' => $section_id
             ]);
             if (!$product_id) continue;
-            
+    
             $result0 = Product::add([
                 'ID' => $product_id,
                 'AVAILABLE' => 'Y',
                 'QUANTITY_TRACE' => 'N',
                 'CAN_BUY_ZERO' => 'Y'
             ]);
-            
+    
             $result1 = Price::add([
                 'PRODUCT_ID' => $product_id,
                 'CATALOG_GROUP_ID' => 1,
