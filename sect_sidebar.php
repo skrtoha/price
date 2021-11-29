f6850500c337bb7271f377fe6446b7130e944100
a.skrichevskij <a.skrichevskij@mebot24.ru>
2021-11-29 12:40:39 +0200

    Разработка

--- sect_sidebar.php
+++ sect_sidebar.php
@@ -78,18 +78,18 @@
 </div>
 
 <div class="mb-5">
-	<?$APPLICATION->IncludeComponent(
-		"bitrix:main.include",
-		"",
-		Array(
-			"AREA_FILE_SHOW" => "file",
-			"PATH" => SITE_DIR."include/twitter.php",
-			"AREA_FILE_RECURSIVE" => "N",
-			"EDIT_MODE" => "html",
-		),
-		false,
-		Array('HIDE_ICONS' => 'N')
-	);?>
+	<?$APPLICATION->IncludeComponent("bitrix:main.include", "", array(
+	"AREA_FILE_SHOW" => "file",
+		"PATH" => SITE_DIR."include/twitter.php",
+		"AREA_FILE_RECURSIVE" => "N",
+		"EDIT_MODE" => "html"
+	),
+	false,
+	array(
+	"HIDE_ICONS" => "N",
+		"ACTIVE_COMPONENT" => "N"
+	)
+);?>
 </div>
 
 <div class="mb-5">
